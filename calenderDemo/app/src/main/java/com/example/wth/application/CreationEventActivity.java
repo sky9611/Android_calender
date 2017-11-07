4eca0e37bff726e2b028f3d818fd40925819df2a
sky9611 <collinbeyond@gmail.com>
2017-11-07 11:43:49 +0100

    first commit

---
+++ calenderDemo/app/src/main/java/com/example/wth/application/CreationEventActivity.java
@@ -0,0 +1,13 @@
+package com.example.wth.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreationEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_event);
+    }
+}
