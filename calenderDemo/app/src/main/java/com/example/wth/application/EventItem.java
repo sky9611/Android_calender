00eccab437e406a6f360854e82fdffd95db9930c
Zhewei Tu <zhewei.tu@insa-lyon.fr>
2017-11-19 22:00:08 +0100

    Revert "Revert "calendar view done""
    This reverts commit 6a7efb0abccb505c93f6be570bb569445666ee96.

---
+++ calenderDemo/app/src/main/java/com/example/wth/application/EventItem.java
@@ -0,0 +1,18 @@
+package com.example.wth.application;
+
+/**
+ * Created by Inspiron on 2017/11/18.
+ */
+
+public class EventItem {
+    private String event;
+    public EventItem(String event) {
+        this.event = event;
+    }
+    public String getEvent() {
+        return event;
+    }
+    public void setEvent(String event) {
+        this.event = event;
+    }
+}
