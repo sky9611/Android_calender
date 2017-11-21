2e656ec61dfb879ff34155ff3d51468f48491e49
lulucasono <lulucasono@gmail.com>
2017-11-21 17:33:22 +0100

    Preset values in the forms

--- calenderDemo/app/src/main/java/com/example/wth/test/AddMember.java
+++ calenderDemo/app/src/main/java/com/example/wth/test/AddMember.java
@@ -1,12 +1,15 @@
 package com.example.wth.test;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
+import com.example.wth.application.CreationGroupActivity;
 import com.example.wth.application.R;
 
 public class AddMember extends AppCompatActivity {
@@ -18,12 +21,12 @@ public class AddMember extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Button fab = (Button) findViewById(R.id.button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(AddMember.this,CreationGroupActivity.class);
+                startActivity(i);
             }
         });
     }
